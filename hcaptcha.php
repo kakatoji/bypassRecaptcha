09e305c3120632e28a1aeee94a10d93f7ff3129d
kakatoji <kakatoji0@gmail.com>
2021-02-15 07:26:08 +0800

    hcaptcha.php

--- hcaptcha.php
+++ hcaptcha.php
@@ -1,10 +1,5 @@
 <?php
 
-//$as=file_get_contents("https://2captcha.com/in.php?key=f05aeb199232adda9275c840ed23abf7&method=hcaptcha&sitekey=7bf8f3af-082a-4e44-877f-b3d00f2ba9cf&pageurl=https://dogeclaim.com/claim");
-//$rs=explode('OK|',$as)[1];
-
-//$get=file_get_contents("https://2captcha.com/res.php?key=f05aeb199232adda9275c840ed23abf7&action=get&id=$rs");
-//echo $get;
 
 function hcaptcha($apiKey,$sitekey,$pageUrl){
     $kirim= file_get_contents("http://2captcha.com/in.php?key=".$apiKey."&method=hcaptcha&sitekey=".$sitekey."&pageurl=".$pageUrl);
